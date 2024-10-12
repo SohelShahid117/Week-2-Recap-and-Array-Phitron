588226581165e39093ce16dd95414c723ae43947
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-12 19:11:45 +0600

    done 6.6 - Continue Statement
     & tomorrow  start from 6.7 digits

--- module-6-loop recap & nested loop/brk.c
+++ module-6-loop recap & nested loop/brk.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 int main(){
-    //print the lowest number from 100-300 which is divided by-2,3,5
+    // print the lowest number from 100-300 which is divided by-2,3,5
     for(int i=100;i<=300;i++){
-        printf("%d\n",i);
         if(i%2==0 && i%3==0 && i%5==0){
             printf("%d is lowest number from 100-300 which is divided by-2,3,5\n",i);
             break;
         }
+        printf("%d\n",i);
+    }
+
+    printf("\n\n\n");
+      for(int i=100;i<=300;i++){
+        if(i%2==0 && i%3==0 && i%5==0){
+            printf("%d is lowest number from 100-300 which is divided by-2,3,5\n",i);
+            // continue;
+            continue;
+        }
+        printf("%d\n",i);
     }
 
     return 0;
--- module-6-loop recap & nested loop/tempCodeRunnerFile.c
+++ module-6-loop recap & nested loop/tempCodeRunnerFile.c
@@ -1,3 +1,7 @@
-for(int j=0;j<3;j++){
-            count++;
-        }
+ // for(int i=100;i<=300;i++){
+    //     printf("%d\n",i);
+    //     if(i%2==0 && i%3==0 && i%5==0){
+    //         printf("%d is lowest number from 100-300 which is divided by-2,3,5\n",i);
+    //         break;
+    //     }
+    // }
